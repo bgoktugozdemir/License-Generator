dc377f1696f45570bd972e2ee149db9795a315ec
Göktuğ Özdemir <bgoktugozdemir@gmail.com>
2018-08-03 22:32:22 +0300

    v0.1.0

---
+++ LicenseGenerator/LicenseGenerator/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace LicenseGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbLicenseKey = new System.Windows.Forms.TextBox();
+            this.tbDateLimit = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbLicenseKey
+            // 
+            this.tbLicenseKey.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.tbLicenseKey.BackColor = System.Drawing.Color.White;
+            this.tbLicenseKey.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tbLicenseKey.ForeColor = System.Drawing.Color.Red;
+            this.tbLicenseKey.Location = new System.Drawing.Point(13, 65);
+            this.tbLicenseKey.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tbLicenseKey.MaxLength = 29;
+            this.tbLicenseKey.Name = "tbLicenseKey";
+            this.tbLicenseKey.ReadOnly = true;
+            this.tbLicenseKey.Size = new System.Drawing.Size(357, 33);
+            this.tbLicenseKey.TabIndex = 0;
+            // 
+            // tbDateLimit
+            // 
+            this.tbDateLimit.Location = new System.Drawing.Point(156, 23);
+            this.tbDateLimit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tbDateLimit.Name = "tbDateLimit";
+            this.tbDateLimit.Size = new System.Drawing.Size(71, 29);
+            this.tbDateLimit.TabIndex = 0;
+            this.tbDateLimit.Text = "0";
+            this.tbDateLimit.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.tbDateLimit.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbDateLimit_KeyPress);
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnGenerate.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnGenerate.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnGenerate.Location = new System.Drawing.Point(377, 65);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(102, 33);
+            this.btnGenerate.TabIndex = 2;
+            this.btnGenerate.Text = "Oluştur";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Black", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Lisans Süresi:";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnGenerate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(491, 105);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.tbDateLimit);
+            this.Controls.Add(this.tbLicenseKey);
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "License Key Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbLicenseKey;
+        private System.Windows.Forms.TextBox tbDateLimit;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ LicenseGenerator/LicenseGenerator/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace LicenseGenerator
+{
+    public partial class Form1 : Form
+    {
+        public SqlConnection Connection;
+        public SqlCommand Command;
+        private int _error = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            SQL();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            string password = GenerateLicense();
+            SaveLicense(password, Convert.ToInt32(tbDateLimit.Text));
+            tbLicenseKey.Text = password;
+        }
+
+        private void SQL()
+        {
+            Connection = new SqlConnection(Properties.Settings.Default.DOVIZConnectionString);
+        }
+
+        private string GenerateLicense()
+        {
+            const string UPPERCASE_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            const string NUMERIC_CHARACTERS = "0123456789";
+            const string SPECIAL_CHARACTERS = @"?!#$%&*@\";
+            const int PASSWORD_LENGTH = 25 + 4;
+
+            string allowedChars = UPPERCASE_CHARACTERS + NUMERIC_CHARACTERS;
+            char[] pass = new char[PASSWORD_LENGTH];
+
+            Random rd = new Random();
+
+            for (int i = 0; i < PASSWORD_LENGTH; i++)
+            {
+                //4'LÜ BLOKLAR
+                //if (i % 5 == 4)
+                //{
+                //    pass[i] = '-';
+                //}
+                //5'Lİ BLOKLAR
+                if (i % 6 == 5)
+                {
+                    pass[i] = '-';
+                }
+                else
+                {
+                    pass[i] = allowedChars[rd.Next(0, allowedChars.Length)];
+                }
+            }
+
+            return new string(pass);
+        }
+
+        private void SaveLicense(string licenseKey, int dateLimit)
+        {
+            Connection.Open();
+
+            try
+            {
+                string query = "INSERT INTO tblLicense(LicenseKey, DateLimit) " +
+                               "VALUES (@LicenseKey, @DateLimit)";
+
+                Command = new SqlCommand(query, Connection);
+                Command.Parameters.AddWithValue("@LicenseKey", licenseKey);
+                Command.Parameters.AddWithValue("@DateLimit", dateLimit);
+
+                Command.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                if (_error >= 5)
+                {
+                    MessageBox.Show("ŞİFRE OLUŞTURULAMADI!", "İşlem Başarısız Oldu", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Connection.Close();
+                    return;
+                }
+                SaveLicense(licenseKey, dateLimit);
+                _error++;
+            }
+            finally
+            {
+                Connection.Close();
+            }
+        }
+
+        private void tbDateLimit_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+        }
+    }
+}
